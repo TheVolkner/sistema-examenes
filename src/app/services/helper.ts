19efe5f273a23788420e7c6cf318d671aac6e2ca
Omar Arvelo <omararvelo81@gmail.com>
2023-02-05 12:55:04 -0400

    Delete helper.ts

--- src/app/services/helper.ts
+++
@@ -1,3 +0,0 @@
-//CREAMOS UN ATRIBUTO QUE NOS SERVIRA COMO PATH BASE PARA HACER LAS PETICIONES HTTP AL BACK-END
-let baseUrl = 'https://sistema-examenes-backend-production.up.railway.app/api';
-export default baseUrl;
